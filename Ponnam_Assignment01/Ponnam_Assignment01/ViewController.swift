abf743df41fcc05ef65f6908c3a3f443751b2e2d
JAGADEESH PONNAM <112121032+Jagadeeshponnam@users.noreply.github.com>
2023-01-31 15:14:38 -0600

    Add files via upload

---
+++ Ponnam_Assignment01/Ponnam_Assignment01/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Ponnam_Assignment01
+//
+//  Created by Ponnam,Jagadeesh on 1/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var firstNameOutlet: UITextField!
+    
+    @IBOutlet weak var lastNameOutlet: UITextField!
+    @IBOutlet weak var yearOutlet: UITextField!
+    
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var initialsLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
